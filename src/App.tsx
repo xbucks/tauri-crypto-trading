d9eaf71c33441cc82381d7a4c5571f434c1baadd
xbucks <jmy.kot@gmail.com>
2023-10-04 04:34:40 -0700

    feat: add filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import "./App.scss";
 
 function App() {
@@ -16,6 +18,12 @@ function App() {
       <div className="app-left">
         <div className="head">Today: 4 Oct 23</div>
         <span className="info">Last Updated: 12 minutes ago</span>
+
+        <ButtonGroup className="filters">
+          <Button variant="primary">Points</Button>
+          <Button variant="secondary">Recent</Button>
+          <Button variant="secondary">Unread</Button>
+        </ButtonGroup>
       </div>
       <div className="app-right">
         <div className="content">
